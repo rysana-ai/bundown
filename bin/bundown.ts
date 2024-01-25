4d4b9c2359f4aa85e13e085fe521c49f89d3c5d4
John <51100181+jwmza@users.noreply.github.com>
2024-01-25 10:52:27 -0500

    0.0.6 pretty-print, help, version commands & colors

--- bin/bundown.ts
+++ bin/bundown.ts
@@ -1,9 +1,55 @@
 #!/usr/bin/env bun
 import { unlink } from 'node:fs/promises'
 import { $, type ShellOutput, file, write } from 'bun'
+import { bold, cyan, gray, magenta } from 'picocolors'
+import { version } from '../package.json'
 
-const usage = '\nbundown <file.md>\n'
-function parse(markdown: string) {
+const usage =
+  `\n${magenta(bold('Bundown'))} is a fast Markdown runtime and bundler. ` +
+  `${gray(`(${version})`)}\n
+${bold(`Usage: bundown <file.md> ${cyan('[...flags]')}`)}\n
+${bold('Flags:')}
+  ${cyan('-p')}, ${cyan('--print')}      Pretty-print source during execution
+  ${cyan('-v')}, ${cyan('--version')}    Print version and exit
+  ${cyan('-h')}, ${cyan('--help')}       Display this menu and exit\n`
+type Flags = Record<string, boolean | string>
+function parseArgs(args: string[]) {
+  if (args.length === 0) return { flags: { help: true } }
+  const output: { path?: string; flags: Flags } = { flags: {} }
+  for (let arg of args) {
+    if (arg[0] === '-') {
+      if (arg[1] !== '-') {
+        switch (arg) {
+          case '-h':
+            arg = '--help'
+            break
+          case '-v':
+            arg = '--version'
+            break
+          case '-p':
+            arg = '--print'
+            break
+        }
+      }
+      output.flags[arg.slice(2)] = true
+      continue
+    }
+    if (!output.path) {
+      output.path = arg
+      continue
+    }
+    console.warn(`Unrecognized argument "${arg}"`)
+  }
+  return output
+}
+function printBlock(content: string, language: string) {
+  return `console.log(\`${
+    (language === 'markdown' ? '\n' : gray(`\n\\\`\\\`\\\`${language}\n`)) +
+    content.trim().replace(/\\/g, '\\\\').replace(/`/g, '\\`').replace(/\${/g, '\\${') +
+    (language === 'markdown' ? '' : gray('\n\\`\\`\\`'))
+  }\`)\n`
+}
+function parseMarkdown(markdown: string, flags: Flags) {
   let state: 'text' | 'code-lang' | 'code-text' = 'text'
   let script = 'import { $ } from "bun"\n\n'
   let block = { language: '', content: '' }
@@ -11,6 +57,7 @@ function parse(markdown: string) {
     switch (state) {
       case 'text':
         if ((j === 0 || markdown[j - 1] === '\n') && markdown.slice(j, j + 3) === '```') {
+          if (flags.print) script += printBlock(block.content, block.language)
           j += 2
           block = { language: '', content: '' }
           state = 'code-lang'
@@ -52,6 +99,7 @@ function parse(markdown: string) {
         break
       case 'code-text':
         if (markdown.slice(j - 1, j + 3) === '\n```') {
+          if (flags.print) script += printBlock(block.content, block.language)
           switch (block.language) {
             case 'typescript':
               script += block.content + '\n'
@@ -67,7 +115,7 @@ function parse(markdown: string) {
               }
               break
             default:
-              console.warn(`Unknown language "${block.language}"`)
+              console.warn(`Unrecognized language "${block.language}"`)
           }
           block = { language: 'markdown', content: '' }
           j += 2
@@ -81,29 +129,32 @@ function parse(markdown: string) {
   return script
 }
 try {
-  const args = process.argv.slice(2)
-  if (args.length === 0) {
+  const { path, flags } = parseArgs(process.argv.slice(2))
+  if (flags.help) {
     console.log(usage)
-    process.exit(1)
+    process.exit(0)
   }
-  const [path] = args
+  if (flags.version) {
+    console.log(version)
+    process.exit(0)
+  }
   if (!path || file(path).size === 0) {
     throw new Error(`File at path "${path}" is empty or not found.`)
   }
   const markdown = await file(path).text()
   const filename = `${process.env.HOME}/.bundown/tmp/${crypto.randomUUID()}.ts`
-  write(filename, parse(markdown))
-  let processShellOutput: ShellOutput | undefined = undefined
+  write(filename, parseMarkdown(markdown, flags))
+  let shellOutput: ShellOutput | undefined = undefined
   try {
-    processShellOutput = await $`bun ${filename}`
-  } catch (processException) {
-    console.error(processException)
+    shellOutput = await $`bun ${filename}`
+  } catch (processError) {
+    console.error(processError)
   } finally {
     await unlink(filename)
-    process.exit(processShellOutput?.exitCode ?? 0)
+    process.exit(shellOutput?.exitCode ?? 0)
   }
-} catch (error) {
+} catch (bundownError) {
   console.log(usage)
-  console.error(error)
+  console.error(bundownError)
   process.exit(1)
 }
